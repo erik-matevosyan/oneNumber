bc51448bd86264b42837da401f65ae8edb6a099f
Grigor Minasyan <grigorminasyan1998@gmail.com>
2021-05-08 08:10:21 -0700

    added rendering html from prod server

--- server/server.js
+++ server/server.js
@@ -3,9 +3,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+
+app.use(express.static(path.join(__dirname, '../build/')));
+
+
 app.get('/', (req, res) => {
-    res.send('bye');
+    res.sendFile(path.join(__dirname, '../build/index.html'));
 });
+
 
 app.listen(port, () => {
     console.log(`Current enviroment is: ${process.env.NODE_ENV}`);
